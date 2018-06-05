6f7a76b3fa3a5cc5b3112c712e3604ad653e04a4
jackrex <jackrex1993@gmail.com>
2018-06-05 19:24:17 +0800

    init

---
+++ addTF.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#coding:utf-8
+__author__ = 'jackrex'
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from Naked.toolshed.shell import muterun_rb
+
+app = Flask(__name__)
+
+result_str = ''
+
+@app.route('/')
+def addUdid():
+    return render_template('upload.html')
+
+
+@app.route('/author')
+def author():
+    return 'Made By Jackrex'
+
+@app.route('/addTF', methods=['POST', 'GET'])
+def call_add_tf_ruby():
+    email = request.form['email']
+    print ('email' + email)
+    success = muterun_rb('udid.rb', email)
+    global result_str
+    if success.exitcode == 0:
+        print ('success')
+        result_str = u'提交成功:'
+    else:
+        print ('error \n')
+        result_str = u'提交失败:'
+
+    return render_template('result.html', result=result_str)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
+
---
+++ udid.rb
@@ -0,0 +1,2 @@
+#!/usr/bin/env ruby
+fastlane pilot add ARGV[0] -a com.gotokeep.keep.intl --username keepvendors@gmail.com -g BetaUsers
